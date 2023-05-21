6013ab12dffd23aa1bbfb29e9bf90732e141289b
Zhcj9527 <1245021768@qq.com>
2023-05-21 17:05:10 +0800

    Vue3定义全局函数和变量

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    
+    {{ $env }}
+  </div>
+  <div>
+    {{ $filters.format('123') }}
   </div>
 </template>
 
 <script setup lang='ts'>
 // import { ref,reactive } from 'vue'
+import { getCurrentInstance } from 'vue';
+
+const instance = getCurrentInstance()
+console.log(instance?.proxy?.$env);
+console.log(instance?.proxy?.$filters.format('jkjk'));
 
 
 
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,28 @@ import testVue from './components/test.vue'
 
 const app = createApp(App)
 // console.log(app);
-
-
+// 定义全局组件
 app.component('Test', testVue) // 组件名，就是引入的组件标签名,,要在挂载之前就全局注册使用
+// 定义全局属性和方法,,***此种方法需要声明declare一下***
+app.config.globalProperties.$env = 'dev'
+
+app.config.globalProperties.$filters = {
+  format<T> (str:T) {
+    return `zhcj-${str}`
+  }
+}
+
+type Filter = {
+  format<T>(str: T): string
+}
+
+declare module 'vue' {
+  export interface ComponentCustomProperties {
+      $filters: Filter,
+      $env: string
+  }
+}
+
 
 
 app.mount('#app')
