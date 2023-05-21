2e0c555c58a6718ae6947e6bede3231732e341b5
Zhcj9527 <1245021768@qq.com>
2023-05-21 20:22:41 +0800

    scoped

---
+++ src/components/scoped/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-input placeholder="test" class="ipt"></el-input>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref,reactive } from 'vue'
+
+</script>
+
+<style scoped lang="less">
+.ipt{
+  width: 300px;
+  margin: 100px 100px;
+  :deep(.el-input__wrapper) {
+    background-color: red;
+  }
+}
+</style>
