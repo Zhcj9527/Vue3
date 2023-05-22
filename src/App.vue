c6ea2bec6f107bb445d9d875dda9004865e959ea
Zhcj9527 <1245021768@qq.com>
2023-05-22 13:47:39 +0800

    others API

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
   <div>
-    <p>actions-user： {{ Test.user }}</p>
+    <p>actions-user: {{ Test.user }}</p>
     <hr>
-    <p>actions-name： {{ Test.name }}</p>
+    <p>actions-name: {{ Test.name }}</p>
     <hr>
     <p>getters: {{ Test.newName }}</p>
     <br><br>
     change: <button @click="change">change</button>
+    <hr>
+    <p>
+      <button @click="reset">reset</button>
+    </p>
   </div>
 </template>
 
 <script setup lang='ts'>
-import { ref,reactive } from 'vue'
+import { ref, reactive } from 'vue'
 import { useTestStore } from './store';
 
 const Test = useTestStore()
 
+ // 类似watchEffect，侦听state的值
+/* Test.$subscribe((args, state) => {
+  console.log('========>',args);
+  console.log('========>',state);
+}) */
+// 
 
 const change = () => {
   // Test.setUser() // 同步
   Test.asyncSetUser() // 异步
 }
 
+const reset = () => {
+  Test.$reset()  // 重置state的值
+}
+
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
