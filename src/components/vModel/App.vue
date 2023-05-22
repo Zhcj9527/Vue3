f751206a6c70e4cb9dc2c8060362a548a6da9fb6
Zhcj9527 <1245021768@qq.com>
2023-05-22 09:34:34 +0800

    深入v-model

---
+++ src/components/vModel/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    App--{{ flag }}
+    <test v-model="flag" v-model:title="title"></test>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref,reactive,provide } from 'vue'
+import test from './components/test.vue';
+
+const flag = ref<number>(4)
+const title = ref<string>('zhcj')
+// provide('flag', flag)
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/vModel/test.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button @click="change">change flag</button>
+    {{ props.modelValue }} ---{{ props.title }}
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref,reactive,inject, Ref } from 'vue'
+
+// const flag = inject<Ref<number>>('flag')
+
+type Props = {
+  modelValue: number,
+  title: string
+}
+
+const props = defineProps<Props>()
+
+const emit = defineEmits(['update:modelValue', 'update:title'])
+
+const change = () => {
+  emit('update:modelValue', 5)
+  emit('update:title', 'zhcajoj')
+}
+
+
+
+
+
+</script>
+
+<style scoped>
+
+</style>
