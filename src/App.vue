5de6924562aac0c247d08e99c6a2842403d3b37d
Zhcj9527 <1245021768@qq.com>
2023-05-22 13:31:50 +0800

    actions/getters

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    origin pinia:{{ Test.current }}
+    <p>actions-user： {{ Test.user }}</p>
     <hr>
-    pinia {{ current }}
+    <p>actions-name： {{ Test.name }}</p>
+    <hr>
+    <p>getters: {{ Test.newName }}</p>
     <br><br>
     change: <button @click="change">change</button>
   </div>
@@ -11,27 +13,13 @@
 <script setup lang='ts'>
 import { ref,reactive } from 'vue'
 import { useTestStore } from './store';
-import { storeToRefs } from 'pinia'
 
 const Test = useTestStore()
-console.log(Test);
-// 更改state的值
-//1  Test.current++
-//2  Test.$patch({current: 888}) 
-//3  Test.$patch((state) => {state.current = 999}) 函数式写法---->推荐
-//4  Test.$state = {current: 852}  这种方式要求全部state更改一下
-//5  Test.setCurrent(456) 通过actions，可以直接使用
-
-// pinia解构 是不具有响应式的  用storeToRefs包裹一下
-const {current} = storeToRefs(Test)
-console.log(current);
-
-
-
 
 
 const change = () => {
-  Test.setCurrent(456)
+  // Test.setUser() // 同步
+  Test.asyncSetUser() // 异步
 }
 
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,58 @@
 import { defineStore } from 'pinia'
 import { Names } from '../store-namespace'
 
+type User = {
+  name: string,
+  age: number
+}
+
+let result: User = {
+  name: 'chjian',
+  age: 456
+}
+
+const Login = (): Promise<User> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        name: 'zhian',
+        age: 789
+      })
+    }, 2000)
+  })
+}
+
 export const useTestStore = defineStore(Names.Test, {
   state: () => {
     return {
-      current: 100
+      user: <User>{}, // 类型断言
+      name: 'hhhh'
     }
   },
   // 类似computed 可以帮助我们去修饰值
   getters: {
-
+    newName(): string {
+      return `$-${this.name}-${this.getUserAge}`
+    },
+    getUserAge(): number {
+      return this.user.age
+    }
   },
   // 可以操作异步 和 同步提交state
   actions: {
-    setCurrent(num: number) {
-      this.current = num
+    // 同步写法
+    setUser() {
+      this.user = result
+    },
+    // 异步的写法
+    async asyncSetUser() {
+      let res = await Login()
+      // console.log(res);
+      this.user = res
+      this.setName(res.name)
+    },
+    setName(name: string) {
+      this.name = name
     }
   }
 })
