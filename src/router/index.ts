7c8c5c3e22bc2bee3be5f2310481d67a75c92a1b
Zhcj9527 <1245021768@qq.com>
2023-05-23 14:55:57 +0800

    滚动行为

--- src/router/index.ts
+++ src/router/index.ts
@@ -40,7 +40,30 @@ const routes:Array<RouteRecordRaw> = [
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  scrollBehavior: (to, from, savedPosition) => {
+    console.log(savedPosition);
+    if(savedPosition) {
+      return savedPosition // 暂存此时滚动条的位置
+    } else {
+      return {
+        top:0 // 这里可以手写指定的位置，比如说top：500
+      }
+    }
+    // 可以返回的是指定的位置
+    /* return {
+      top:500
+    } */
+    // 返回的可以是异步
+    /* return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          top: 400
+        })
+      }, 2000);
+    }) */
+    
+  },
+  routes,
 })
 
 
